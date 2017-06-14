f89ed24db70709d9f011e5c3e0c4ac472e85034c
Tangati, Lavanya <ltangati@paypal.com>
2017-06-14 18:03:00 -0500

    User story 5 Events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+50.times do
+  registered_application = RegisteredApplication.create!(
+    name: Faker::Name.title,
+    url: Faker::Internet.url
+  )
+end
+registered_applications = RegisteredApplication.all
+
+100.times do
+  Event.create!(
+    registered_application: registered_applications.sample,
+    name: Faker::Name.title
+  )
+end
+
+puts "Seed finished"
+puts "#{RegisteredApplication.count} registrations created"
+puts "#{Event.count} events created"
---
+++ spec/factories/events.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :event do
+    name "MyText"
+    registered_application nil
+  end
+end
