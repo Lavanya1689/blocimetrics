116353c38c1bf891ec6e69c9ba573e223ac78853
Tangati, Lavanya <ltangati@paypal.com>
2017-06-29 15:26:32 -0500

    api work

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-
   resources :registered_applications do
     resources :events, only: [:create, :destroy]
+  end
+
+  namespace :api, defaults: {format: :json} do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
   end
 
   devise_for :users
